0589c2619395b8d708b4601dcc6be2039ee54ecc
Jeffery Zhang <jeffery.zhang@sleepcare365.com>
2019-03-06 00:27:49 -0800

    add user model

--- app/core/models.py
+++ app/core/models.py
@@ -4,9 +4,20 @@ PermissionsMixin
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address!')
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
@@ -22,3 +33,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
