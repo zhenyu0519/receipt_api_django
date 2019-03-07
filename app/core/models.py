3c6cedf50d833689273e077d64269500013592b6
Jeffery Zhang <jeffery.zhang@sleepcare365.com>
2019-03-06 23:39:10 -0800

    git add .

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-PermissionsMixin
+    PermissionsMixin
+
 
 class UserManager(BaseUserManager):
+
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address!')
@@ -10,7 +12,6 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, password):
@@ -19,18 +20,15 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+
     """Customer user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=True)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
+
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@londonappdev.com'
@@ -10,7 +12,6 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -31,6 +32,5 @@ class ModelTests(TestCase):
             'test@londonappdev.com',
             'test123'
         )
-
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
